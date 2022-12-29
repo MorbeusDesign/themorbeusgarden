fe4d097f826ad3e04b8628a543ff7bccde9b3bfe
Morbeus <fabio.trupia@gmx.net>
2022-12-29 21:07:42 +0100

    Some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <Container>
         <nav className="flex justify-between py-6 md:py-10">
           <Link to="/" className="text-black">
-            Obsidian Garden
+            The Morbeus Garden
           </Link>
           <ul className="list-style-none grid grid-flow-col auto-cols-max gap-6 items-center">
             {MENU.map((item) => (
@@ -19,9 +19,9 @@ export default function Header() {
             ))}
             <li>
               <Link
-                to="https://github.com/juxtdesigncc/gatsby-starter-obsidian-garden"
+                to="https://github.com/MorbeusDesign/themorbeusgarden"
                 target="_blank"
-                className="px-4 py-2 bg-gray-800 rounded-full text-white hover:bg-gray-600 transition-all ease-in-out duration-300 hover:text-white uppercase"
+                className="mx-auto px-4 py-2 bg-gray-800 rounded-full text-white hover:bg-gray-600 transition-all ease-in-out duration-300 hover:text-white uppercase"
               >
                 Try Now
               </Link>
