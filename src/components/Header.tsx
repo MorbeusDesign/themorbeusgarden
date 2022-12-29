2cff1aaf7f79b2ec3f93de3cb2d75fef6a7fb569
Morbeus <fabio.trupia@gmx.net>
2022-12-29 22:10:27 +0100

    pe10

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header>
       <Container>
         <nav className="flex justify-between py-6 md:py-10">
-          <Link to="/" className="text-black">
+          <Link to="/" className="text-black sm:pe-10">
             TheMorbeusGarden
           </Link>
           <ul className="list-style-none grid grid-flow-col auto-cols-max gap-6 items-center">
